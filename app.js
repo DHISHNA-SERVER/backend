da0748e74a1e115a9f2e84419cfd9880bac7b110
Jyothis-P <jyothisp52@gmail.com>
2019-12-21 22:33:09 +0530

    app express

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 8000
 
-const hostname = '127.0.0.1';
-const port = 8000;
+app.get('/', (req, res) => res.send('Hello World!'))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
